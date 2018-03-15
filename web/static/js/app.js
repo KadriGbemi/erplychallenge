94676bfe430812c02272fe649219394866b1f365
John Doe <kadri.oluwagbemi@gmail.com>
2018-03-15 21:33:24 +0200

    Updates

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,16 +1,14 @@
 import Vue from "vue";
 
-import BootstrapVue from 'bootstrap-vue';
-
-Vue.use(BootstrapVue);
 
 import axios from "axios";
 var product = new Vue({
     el:'#shop-app',
     data: {
-        searchbyinstock: 'true',
         listsgrid: [],
         errors: [],
+        cart: [],
+        currentitem:{},
         searchbycountry: "", 
     },
     computed: {
@@ -22,25 +20,54 @@ var product = new Vue({
                 return b.name - a.name;
             }) 
             return orderbycountry;
-            },
-        }, 
-         
-        created: function () {
+            },    
+    }, 
+        mounted: function () {
+            console.log("1....");
              this.load();
         },
         methods: {
+            addtocart: function (country) {
+                this.currentitem = country;
+                var found = false;
+                var item = {product:country, quantity:1}
+                this.cart.forEach(cartItem=>{
+                    if (cartItem.product.id==country.id){
+                        cartItem.quantity++;
+                        found = true;
+                    }
+                });
+                if(!found){
+                    this.cart.push(item); 
+                }
+                this.getCartSize();
+               },
+            getCartSize: function(){
+                   console.log("Size",this.cart.length);
+                     return this.cart.length;
+               },
+
+            // getCartSizeitem: function(count){
+            //    var count = 0;
+            //    count++;
+            // }, 
+          
+               removefromcart:function(country){
+                this.currentitem = country;
+                   this.cart.splice(this.cart.indexOf(country),1);
+               },
             load: function () {          
-            var product = this;
-               axios.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d')
+                var product = this;
+                console.log("1....");
+                axios.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d')
                .then(function (response){
                     product.listsgrid = (response.data);
-                    const listsgrid = product.listsgrid; 
-                   console.log(listsgrid);
+                    listsgrid = product.listsgrid; 
+                   console.log(listsgrid[0].name);
                })
                .catch(function (error){
-                    product.errors.push(e);
+                    product.errors.push(error);
                })
            } 
        }
-
   });
