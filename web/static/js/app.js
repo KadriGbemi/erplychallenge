63c17090e4d9f554c92e0406f855f594c737d726
John Doe <kadri.oluwagbemi@gmail.com>
2018-03-18 06:01:57 +0200

    Update on Erply frontend challenge

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,19 +1,30 @@
-import Vue from "vue";
+import Vue from "vue"
 
+import axios from "axios"
 
-import axios from "axios";
+import VuePaginate from 'vue-paginate'
+
+Vue.use(VuePaginate)
+
+Vue.component('modal', {
+    template: '#modal-template'
+})
+
 var product = new Vue({
     el:'#shop-app',
     data: {
-        listsgrid: [],
         errors: [],
         cart: [],
+        item: '',
         currentitem:{},
-        searchbycountry: "", 
+        searchbycountry: "",
+        showModal: false,
+        itemsdata: [],
+        paginate: ['itemsdata']
     },
     computed: {
         findbycountry: function () {
-            let findbycountry = this.listsgrid.filter((country) => {
+            let findbycountry = this.itemsdata.filter((country) => {
                 return country.store.toLowerCase().includes(this.searchbycountry.toLowerCase()) && country.instock == true 
             }) 
             let orderbycountry= findbycountry.sort((a, b) => {
@@ -23,16 +34,16 @@ var product = new Vue({
             },    
     }, 
         mounted: function () {
-            console.log("1....");
              this.load();
         },
+      
         methods: {
-            addtocart: function (country) {
-                this.currentitem = country;
+            addtocart: function (items) {
+                this.currentitem = items;
                 var found = false;
-                var item = {product:country, quantity:1}
-                this.cart.forEach(cartItem=>{
-                    if (cartItem.product.id==country.id){
+                var item = {product:items, quantity:1}
+                var eachitem = this.cart.forEach(cartItem=>{
+                    if (cartItem.product.id == items.id){
                         cartItem.quantity++;
                         found = true;
                     }
@@ -43,31 +54,42 @@ var product = new Vue({
                 this.getCartSize();
                },
             getCartSize: function(){
-                   console.log("Size",this.cart.length);
                      return this.cart.length;
+               },   
+               removeallfromcart:function(items){
+                this.currentitem = items;
+                var allofcart = this.cart.splice(this.cart.indexOf(items),1);
+                //this.cart = [];
+                alert("Your cart is empty");
                },
-
-            // getCartSizeitem: function(count){
-            //    var count = 0;
-            //    count++;
-            // }, 
-          
-               removefromcart:function(country){
-                this.currentitem = country;
-                   this.cart.splice(this.cart.indexOf(country),1);
+               removeonefromcart: function(items){
+                this.currentitem = items;
+                var available = false;
+                var oneitem = {product:items, quantity: 1}
+                var eachitem = this.cart.forEach(cartitem=>{
+                    if (cartitem.product.id == items.id){
+                        cartitem.quantity--;
+                        available = true;
+                        console.log("First", cartitem.quantity);
+                        if (cartitem.quantity < 0 || cartitem.quantity == 0) {
+                            var output = "Product removed";
+                            cartitem.quantity = output;
+                        }
+                    }
+                });
                },
-            load: function () {          
+            load: function () { 
                 var product = this;
-                console.log("1....");
                 axios.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d')
                .then(function (response){
-                    product.listsgrid = (response.data);
-                    listsgrid = product.listsgrid; 
-                   console.log(listsgrid[0].name);
+                    product.itemsdata = (response.data);
+                    itemsdata = product.itemsdata;
+                    return itemsdata; 
+               console.log("See items", itemsdata[0])
                })
                .catch(function (error){
                     product.errors.push(error);
                })
-           } 
+           }
        }
   });
