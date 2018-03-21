505481c82b509bd7eaeb78d06fbc8f7a4318e7c3
John Doe <kadri.oluwagbemi@gmail.com>
2018-03-21 08:19:54 +0200

    Update

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -5,11 +5,7 @@ import axios from "axios"
 import VuePaginate from 'vue-paginate'
 
 Vue.use(VuePaginate)
-
-Vue.component('modal', {
-    template: '#modal-template'
-})
-
+  
 var product = new Vue({
     el:'#shop-app',
     data: {
@@ -21,34 +17,31 @@ var product = new Vue({
         search: '',
         currentitem:{},
         searchbycountry: "",
-        showModal: false,
         itemsdata: [],
         recentitem:[],
         paginate: ['itemsdata'],
         show: true,
-        seen: true
+        seen: true,
+
     
     },
-    computed: {
-        findbycountry: function () {
-            let findbycountry = this.itemsdata.filter((country) => {
-                return country.store.toLowerCase().includes(this.searchbycountry.toLowerCase()) && country.instock == true 
+    watch: {
+        cart: {
+          handler() { console.log('Cart added!'); },
+          deep: true,
+        }
+      },
+      computed:{
+        paginated: function (country) {
+           let paginated = this.itemsdata.filter((item) => {
+                return item.store.toLowerCase().includes(this.searchbycountry.toLowerCase()) && item.instock == true
             }) 
-            let orderbycountry= findbycountry.sort((a, b) => {
+            let orderbycountry= paginate.sort((a, b) => {
                 return b.name - a.name;
             }) 
             return orderbycountry;
-            },
-            carttotal: function (){ 
-               
-              let carttotal =  this.currentitem;
-              var total = 0;             
-                for(var i = 0; i < this.carttotal.length; i++) {
-                    total += this.carttotal[i].price;
-                }
-                return carttotal;
-              },
-     }, 
+        },
+      },
         mounted: function () {
              this.load();
         },
@@ -56,46 +49,53 @@ var product = new Vue({
         methods: {
             addtocart: function (items) {
                 this.currentitem = items;
-                var found = false;
-                var item = {product:items, quantity:1}
+                 var found = false;
+                var item = {product:items, quantity:1};
                 var eachitem = this.cart.forEach(cartItem=>{
-                    if (cartItem.product.id == items.id){
-                        cartItem.quantity++;
-                        found = true;
+                    if (cartItem.product.id === items.id){
+                       var output= cartItem.quantity++;
+                      found = true;
+                        
                     }
                 });
                 if(!found){
-                    this.cart.push(item); 
+                    this.cart.push(item);
+                  //  this.saveCart();
                 }
-                this.getCartSize();
+                this.getCartSize(item);
+
                },
             getCartSize: function(){
-                     return this.cart.length;
-               },   
-               removeallfromcart:function(items){
-                this.currentitem = items;
-                var allofcart = this.cart.splice(this.cart.indexOf(items),1);
-                   return alert("Your cart will be empty");
-               },
-
-            removeonefromcart: function(item) {
-                this.recentitem = item;
-                if (item.quantity > 0){
-                    item.quantity -=1;
-                }
-                    if(cart.length === 0){
-                    {
-                    var output="Your cart is empty";
-                    return alert("Your cart will be empty");
+                return this.cart.length;
+               }, 
+                      
+            carttotal: function (){ 
+                   var total = 0;
+                     for(var i = 0; i < this.cart.length; i++){
+                         total += this.cart[i].product.price * this.cart[i].quantity;
                     }
-                }
+                return total;   
             },
+            removeonefromcart: function(item) {
+                this.removeitem = item;
+                var allofcart = this.cart.splice(this.cart.indexOf(item),1);
+                localStorage.removeItem(item);
+                this.saveCart();
+            },
+            removeallfromcart:function(items){
+                this.current = items;
+                this.cart = [];
+                return alert("Your cart will be empty");
+                localStorage.removeItem(items);
+                this.saveCart();
+               },    
             load: function () { 
                 var product = this;
                 axios.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d')
                .then(function (response){
                     product.itemsdata = (response.data);
                     itemsdata = product.itemsdata;
+                    localStorage.setItem(itemsdata);
                     return itemsdata; 
 
                    console.log("See items", itemsdata)
@@ -107,6 +107,7 @@ var product = new Vue({
                .catch(function (error){
                     product.errors.push(error);
                })
-           }
+           },
+          
        }
   });
