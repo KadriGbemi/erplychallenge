c0c6103322218857830caf9fe424a050c877d13e
John Doe <kadri.oluwagbemi@gmail.com>
2018-03-22 01:26:43 +0200

    Update

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -25,22 +25,16 @@ var product = new Vue({
 
     
     },
-    watch: {
-        cart: {
-          handler() { console.log('Cart added!'); },
-          deep: true,
-        }
-      },
       computed:{
-        paginated: function (country) {
-           let paginated = this.itemsdata.filter((item) => {
+        filteredItems() {
+            return this.itemsdata.filter(item => {
                 return item.store.toLowerCase().includes(this.searchbycountry.toLowerCase()) && item.instock == true
-            }) 
+            })
             let orderbycountry= paginate.sort((a, b) => {
                 return b.name - a.name;
             }) 
             return orderbycountry;
-        },
+          }
       },
         mounted: function () {
              this.load();
@@ -54,54 +48,68 @@ var product = new Vue({
                 var eachitem = this.cart.forEach(cartItem=>{
                     if (cartItem.product.id === items.id){
                        var output= cartItem.quantity++;
-                      found = true;
-                        
+                      found = true;           
                     }
                 });
                 if(!found){
                     this.cart.push(item);
-                  //  this.saveCart();
-                }
+                }           
                 this.getCartSize(item);
-
+                this.saveCart();
                },
-            getCartSize: function(){
+              getCartSize: function(){
                 return this.cart.length;
                }, 
-                      
-            carttotal: function (){ 
+               saveCart: function(){
+                localStorage.setItem("cart", JSON.stringify(this.cart));
+               },
+                carttotal: function (){ 
                    var total = 0;
                      for(var i = 0; i < this.cart.length; i++){
                          total += this.cart[i].product.price * this.cart[i].quantity;
                     }
-                return total;   
+                return total;  
+            },
+            increaseitem: function(item){
+                    this.increaseitem = item;
+                    var reduce = item.quantity += 1;
+                    return reduce;              
+                    this.saveCart();                  
+            },
+            reduceitem: function(item){
+                this.removeitem = item;
+                if(item.quantity > 1){
+                    var reduce = item.quantity -= 1;              
+                }
+                else {
+                    this.removeonefromcart(item);
+                }              
+                this.saveCart();
             },
             removeonefromcart: function(item) {
                 this.removeitem = item;
-                var allofcart = this.cart.splice(this.cart.indexOf(item),1);
-                localStorage.removeItem(item);
+                var allofcart = this.cart.splice(this.cart.indexOf(item), 1);
                 this.saveCart();
             },
             removeallfromcart:function(items){
                 this.current = items;
                 this.cart = [];
-                return alert("Your cart will be empty");
-                localStorage.removeItem(items);
                 this.saveCart();
                },    
             load: function () { 
+               if(localStorage.getItem("cart")!=null){
+                     this.cart = JSON.parse(localStorage.getItem("cart"));
+                 }
                 var product = this;
                 axios.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d')
                .then(function (response){
                     product.itemsdata = (response.data);
                     itemsdata = product.itemsdata;
-                    localStorage.setItem(itemsdata);
+                    localStorage.setItem("items",itemsdata);
                     return itemsdata; 
 
-                   console.log("See items", itemsdata)
                    var recentitem = this.product.itemsdata;
                    return recentitem;
-
                    
                })
                .catch(function (error){
